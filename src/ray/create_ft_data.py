308143874929898721d8993588611454166c13e2
cemrtkn <cemrtkn@gmail.com>
2025-02-27 16:27:04 +0100

    changed ft data structure to only include one text column and added a system prompt

--- src/ray/create_ft_data.py
+++ src/ray/create_ft_data.py
@@ -11,25 +11,23 @@ path_to_save = "/mnt/ceph/storage/data-tmp/current/kavo1286/personal/data/ft_dat
 @ray.remote
 class FileProcessor:
     def process(self, match_file, path_to_match, context_length=10):
+        system_prompt = "This is a sequence of football match events."
         match_id = match_file.split(".")[0]
         match_df = pd.read_csv(os.path.join(path_to_match, match_file))
 
-        ft_data = {"match_id": [], "input": [], "output": []}
+        ft_data = {"match_id": [], "text": []}
 
         for id, row in match_df.iterrows():
-            if id > 0:
-                context_start = max(0, id - context_length)
-                if id < 9:
-                    context = "[match_start]\n" + "\n".join(match_df.iloc[context_start:id]['text'])
-                else:
-                    context = "\n".join(match_df.iloc[context_start:id]['text'])
+            context_start = max(0, id - context_length)
+            context = "\n" + "\n".join(match_df.iloc[context_start:id]['text'])   
+
+            if id > 0:                  
+                text = f"{system_prompt}{context}\n{row['text']}"
             else:
-                context = "[match_start]"
-
-
+                text = f"{system_prompt}\n{row['text']}"
+            
             ft_data["match_id"].append(match_id)
-            ft_data["input"].append(context)
-            ft_data["output"].append(row['text'])
+            ft_data["text"].append(text)
 
         return ft_data
 
--- src/ray/test.py
+++ src/ray/test.py
@@ -7,26 +7,21 @@ sample_path = '../../data/small/'
 file_name = '15946.csv'
 
 def process(match_file, path_to_match, context_length=10):
-        system_prompt = "This is a sequence of football match events.\n"
+        system_prompt = "This is a sequence of football match events."
         match_id = match_file.split(".")[0]
         match_df = pd.read_csv(os.path.join(path_to_match, match_file))
 
         ft_data = {"match_id": [], "text": []}
 
         for id, row in match_df.iterrows():
-            if id > 0:
-                context_start = max(0, id - context_length)
-                if id < 9:
-                    context = "[match_start]\n" + "\n".join(match_df.iloc[context_start:id]['text'])                     
-                else:
-                    context = "\n".join(match_df.iloc[context_start:id]['text'])
+            context_start = max(0, id - context_length)
+            context = "\n" + "\n".join(match_df.iloc[context_start:id]['text'])   
+
+            if id > 0:                  
+                text = f"{system_prompt}{context}\n{row['text']}"
             else:
-                context = "[match_start]"
-
-
-            text = f"{system_prompt}{context}\n{row['text']}"
-            if id == len(match_df)-1:
-                 text += "\n[match_end]"
+                text = f"{system_prompt}\n{row['text']}"
+            
             ft_data["match_id"].append(match_id)
             ft_data["text"].append(text)
 
@@ -34,5 +29,7 @@ def process(match_file, path_to_match, context_length=10):
 
 ft_data = pd.DataFrame(process(file_name, sample_path))
 
+print('-'*40)
 print(ft_data['text'][0])
-print(ft_data['text'][3163])
+print('-'*40)
+print(ft_data['text'][len(ft_data)-1])
